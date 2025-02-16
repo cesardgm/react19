44da04869ddcae3de241dd2da1d34a0fb0b32903
Cesar Gonzalez <cesardgm@berkeley.edu>
2025-02-16 00:06:59 -0800

    feat: Added animation to gallery images upon render using react's Motion (formerly framer motion)
    - Simply wrapped the Entry component in Gallery.jsx with an AnimatePresence and motion.div elements.

--- animal-kingdom/src/components/Gallery.jsx
+++ animal-kingdom/src/components/Gallery.jsx
@@ -1,8 +1,22 @@
 import Entry from "./Entry.jsx";
+import { AnimatePresence, motion } from "motion/react";
 
 export default function Gallery({ searchedPictures }) {
-	const entryElements = searchedPictures.map((entry) => {
-		return <Entry key={entry.id} {...entry} />
+	const entryElements = searchedPictures.map((entry, i) => {
+		return (
+			<AnimatePresence>
+				<motion.div 
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition= {{
+						delay: i * 0.3,
+					}}
+					key={entry.id}
+				>
+					<Entry key={entry.id} {...entry} />
+				</motion.div>
+			</AnimatePresence>
+			)
 	});
 
 	return <main className="container">{entryElements}</main>;
